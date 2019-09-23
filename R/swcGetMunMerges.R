33e5965f62e2c182c1f454a9f8f39932bb957895
Thomas Knecht <t.knecht@hotmail.com>
2019-09-23 17:01:29 +0200

    Add swcGetMerges function

---
+++ R/swcGetMunMerges.R
@@ -0,0 +1,45 @@
+swcGetMerges <- function(year = NULL, canton = NULL) {
+  if (is.null(canton)) {
+    mutations <- swcGetMutations()
+  } else {
+    mutations <- swcGetMutations(canton = canton)
+  }
+
+  mun.mut.year <- subset(
+    mutations,
+    mMutationDate >= as.Date(paste0(year, "-01-01")) &
+      mMutationDate < as.Date(paste0(year, "-12-31"))
+  )
+
+  mun.mut.all <- merge(
+    mun.mut.year,
+    mun.mut.year,
+    by.x = "mHistId.y", by.y = "mHistId.x", all = T, incomparables = NA
+  )
+
+  mun.mut <- subset(mun.mut.all, !is.na(get("mMutationNumber.x")))
+
+  mun.mut.red <- filter(
+    mun.mut,
+    !(mMutationNumber.x %in% unique(na.omit(mun.mut$mMutationNumber.y)))
+  )
+
+  mun.mut.fus <- filter(
+    mun.mut.red,
+    !grepl(c("Reassignment|name"), mAdmissionMode.x)
+  )
+
+
+  mun.mut.fus <- mutate(
+    mun.mut.fus,
+    name_to = ifelse(!is.na(mShortName.y.y), mShortName.y.y, mShortName.y.x)
+  )
+
+  mun.mut.final <- select(mun.mut.fus,
+    canton = cAbbreviation.x,
+    mId_from = mId.x.x,
+    name_from = mShortName.x.x,
+    mId_to = mId.y.x,
+    name_to
+  )
+}
